1a0681fc9139514e159880d806ba205254cd3757
Shahid-cs22 <mohamedshahid.cs22@stellamaryscoe.edu.in>
2025-08-16 11:19:23 +0530

    typescript

---
+++ typescript-project1/src/app1.tsx
@@ -0,0 +1,10 @@
+import { useRef } from "react";
+
+function App1() {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  return (
+   <div className="one"> Mohamed Shahid</div>
+  );
+}
+export default App1
